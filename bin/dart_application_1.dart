e72fed04a3e7d805a486f4259f5fda52d009beb2
AyaT310 <ayatarek.edu@gmail.com>
2023-09-20 12:49:05 +0200

    Mahmoud

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,63 @@
+
+import 'dart:ffi';
+import 'dart:io';
+void main() {
+  /*print("please enter your name..");
+  String? name = stdin.readLineSync();
+
+  print("please enter your age");
+  int? age =int.parse(stdin.readLineSync()!);
+  print("mr ${name} your age is ${age}");*/
+
+
+
+
+
+/*double? result;p
+print("please enter the first num");
+double? num1 = double.parse(stdin.readLineSync()!);
+
+print("please enter the second num");
+double? num2 = double.parse(stdin.readLineSync()!);
+
+print("please enter the operator");
+String? operation =stdin.readLineSync();
+
+switch(operation)
+{
+  case "+":
+    result = num1 + num2;
+    break;
+  
+  case "-":
+    result = num1 -num2;
+    break;
+
+  case "/":
+    result = num1 / num2;
+    break;
+
+  case "*":
+    result = num1 * num2;
+    break;    
+
+    default 
+    
+}
+  print("the result=${result}");*/
+
+
+
+
+
+/*List <int> L1 = [1,2,3,4,5];
+List <int> L2 = [1,2,3,4,5];
+
+  for(int i = 0 ; i<L1.length ; i++)
+  {
+    
+      print(i[L1]);
+  }*/
+  
+
+}
